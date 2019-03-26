8b24f49b06571e01cfc3d44133ef6e857825afa7
Bartek Bugała <bartek.bugala@gmail.com>
2019-03-27 00:01:35 +0100

    Edit script.js - comments and fix

--- script.js
+++ script.js
@@ -21,20 +21,28 @@ button.addEventListener('click', function () {
     // declaration of iteration variable
     var i = 5;
     while ((typeof userName !== 'string' || userName === '' || !isNaN(userName)) && i > 0) {
-        var triesString = 'Tries left: ';
+        // declaring const
+        const triesString = 'Tries left: ';
+        // Checking if input is not a string!
         if (typeof userName !== 'string') {
             userName = window.prompt('(Input: ' + typeof (userName) + ') ' + 'No Input! What is your real name?' + triesString + i);
-        } else if (userName === '') {
+        } 
+        // Checking if input is empty (no input)
+        else if (userName === '') {
             userName = window.prompt('(Input: ' + typeof (userName) + ') ' + 'Empty field! What is your real name?' + triesString + i);
-        } else if (!isNaN(userName)) {
+        }
+        // Checking if input is a number (not isNaN)
+         else if (!isNaN(userName)) {
             userName = window.prompt('(Input: ' + typeof (userName) + ' consisting of a number) ' + 'A Number? Are you an alien? What is your real name?' + triesString + i);
         }
         i--;
     }
 
+    // If the iteration finished and tehre is still no input a value is assigned to userName variable
     if ((typeof userName !== 'string' || userName === '' || !isNaN(userName)) && i === 0) {
         userName = 'Mute Individual';
     }
  
+    // Display Message With user name @userName
     output.innerHTML = 'Hello ' + userName + '!' + '<br><br>' + output.innerHTML;
 });
